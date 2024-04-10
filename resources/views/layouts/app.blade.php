435f8da1db45a83576d7b196ac6ab4f1f6ffded0
Johao Lema <johaolema@gmail.com>
2024-04-10 17:25:13 -0500

    Correcion de vista Home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>@yield('title', 'App Shop')</title>
+    <title>@yield('title', 'A&C Otavalo')</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
