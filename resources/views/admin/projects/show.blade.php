f4ace672d214b7ea14363f16966e0ba0f5e1173f
Johao Lema <johaolema@gmail.com>
2024-04-11 21:21:48 -0500

    show/index de tramites

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -12,56 +12,13 @@
     <div class="main main-raised">
         <div class="container">
             <div class="section text-center">
-                <h2 class="title">Información del proyecto "{{ $process->name }}"</h2>
+                <h2 class="title">Información del proyecto "{{ $project->name }}"</h2>
 
                 <div class="team">
                     <div class="row">
                         <a href="{{ url('/admin/processes/create') }}" class="btn btn-primary btn-round">Nuevo trámite</a>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th class="text-center">#</th>
-                                    <th>Tipo de trámite</th>
-                                    <th>VSM</th>
-                                    <th class="col-xs-2 text-center">Próxima revisión</th>
-                                    <th class="text-center">Opciones</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($processes as $process)
-                                    <tr>
-                                        <td class="text-center">{{ $process->id }}</td>
-                                        <td class="text-left">{{ $process->type_process->name }}</td>
-                                        <td class="text-left">{{ $process->type_process->vsm }}</td>
-                                        <td>{{ $process->type_process->next_review }}</td>
-                                        <td class="text-right">&dollar; {{ $process->total_value }}</td>
-                                        <td class="td-actions text-right">
-                                            <form method="post" action="{{ url('/admin/processes/' . $process->id) }}">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-
-                                                <a href="{{ url('/admin/processes/' . $process->id . '/show') }}"
-                                                    type="button" rel="tooltip" title="Ver proyecto"
-                                                    class="btn btn-info btn-simple btn-xs">
-                                                    <i class="fa fa-info"></i>
-                                                </a>
-
-                                                <a href="{{ url('/admin/processes/' . $process->id . '/edit') }}"
-                                                    type="button" rel="tooltip" title="Editar producto"
-                                                    class="btn btn-success btn-simple btn-xs">
-                                                    <i class="fa fa-edit"></i>
-                                                </a>
-                                                <button type="submit" rel="tooltip" title="Eliminar"
-                                                    class="btn btn-danger btn-simple btn-xs">
-                                                    <i class="fa fa-times"></i>
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        {{ $processes->links() }}
+                        
+                        
                     </div>
                 </div>
 
